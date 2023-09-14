fcd2967bdd48f581c0e38829a3741b6424c07306
Joseph Adams <joseph.adams@blueyonder.co.uk>
2023-09-14 16:14:41 +0100

    Added summarising endpoints

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -6,6 +7,12 @@ const port = 4000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 mongoose.connect(
   "mongodb+srv://testDBUser:gd5R7PJ75MJXqrXN@webervice.yhwacfo.mongodb.net/AccidentData?retryWrites=true&w=majority"
@@ -52,6 +59,83 @@ app.get("/historic/borough/:name", async (req, res) => {
   }
 });
 
+app.get("/historic/borough/:name/summary", async (req, res) => {
+  console.log("in summary for " + req.params.name);
+  class BoroughSummary {
+    constructor(
+      year,
+      month,
+      CYC_KILL,
+      CYC_INJD,
+      MOTO_KILL,
+      MOTO_INJD,
+      PEDS_KILL,
+      PEDS_INJD,
+      PERS_KILL,
+      PERS_INJD,
+      NUM_COLS
+    ) {
+      this.year = year;
+      this.month = month;
+      this.CYC_KILL = CYC_KILL;
+      this.CYC_INJD = CYC_INJD;
+      this.MOTO_KILL = MOTO_KILL;
+      this.MOTO_INJD = MOTO_INJD;
+      this.PEDS_KILL = PEDS_KILL;
+      this.PEDS_INJD = PEDS_INJD;
+      this.PERS_KILL = PERS_KILL;
+      this.PERS_INJD = PERS_INJD;
+      this.NUM_COLS = NUM_COLS;
+    }
+  }
+
+  const summaries = {};
+  try {
+    const boroughData = await accidentDataModel.find({
+      BOROUGH: req.params.name,
+    });
+
+    boroughData.forEach((item) => {
+      const year = item.YEAR;
+      const month = item.MONTH;
+      const key = `${year}=${month}`;
+
+      if (!summaries[key]) {
+        summaries[key] = new BoroughSummary(
+          year,
+          month,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0
+        ); // Initialize with zeros
+      }
+
+      // Aggregate the values
+      summaries[key].CYC_KILL += item.CYC_KILL;
+      summaries[key].CYC_INJD += item.CYC_INJD;
+      summaries[key].MOTO_KILL += item.MOTO_KILL;
+      summaries[key].MOTO_INJD += item.MOTO_INJD;
+      summaries[key].PEDS_KILL += item.PEDS_KILL;
+      summaries[key].PEDS_INJD += item.PEDS_INJD;
+      summaries[key].PERS_KILL += item.PERS_KILL;
+      summaries[key].PERS_INJD += item.PERS_INJD;
+      summaries[key].NUM_COLS += item.NUM_COLS;
+    });
+
+    const summaryArray = Object.values(summaries);
+
+    res.json(summaryArray);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 app.get("/historic/borough/:name/:year", async (req, res) => {
   try {
     const boroughData = await accidentDataModel.find({
@@ -59,6 +143,67 @@ app.get("/historic/borough/:name/:year", async (req, res) => {
       YEAR: req.params.year,
     });
     res.json(boroughData);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.get("/historic/borough/:name/:year/summary", async (req, res) => {
+  class BoroughSummary {
+    constructor(
+      month,
+      CYC_KILL,
+      CYC_INJD,
+      MOTO_KILL,
+      MOTO_INJD,
+      PEDS_KILL,
+      PEDS_INJD,
+      PERS_KILL,
+      PERS_INJD,
+      NUM_COLS
+    ) {
+      this.month = month;
+      this.CYC_KILL = CYC_KILL;
+      this.CYC_INJD = CYC_INJD;
+      this.MOTO_KILL = MOTO_KILL;
+      this.MOTO_INJD = MOTO_INJD;
+      this.PEDS_KILL = PEDS_KILL;
+      this.PEDS_INJD = PEDS_INJD;
+      this.PERS_KILL = PERS_KILL;
+      this.PERS_INJD = PERS_INJD;
+      this.NUM_COLS = NUM_COLS;
+    }
+  }
+
+  const summaries = {};
+
+  try {
+    const boroughData = await accidentDataModel.find({
+      BOROUGH: req.params.name,
+      YEAR: req.params.year,
+    });
+
+    boroughData.forEach((item) => {
+      const month = item.MONTH;
+      if (!summaries[month]) {
+        summaries[month] = new BoroughSummary(month, 0, 0, 0, 0, 0, 0, 0, 0, 0); // Initialize with zeros
+      }
+
+      // Aggregate the values
+      summaries[month].CYC_KILL += item.CYC_KILL;
+      summaries[month].CYC_INJD += item.CYC_INJD;
+      summaries[month].MOTO_KILL += item.MOTO_KILL;
+      summaries[month].MOTO_INJD += item.MOTO_INJD;
+      summaries[month].PEDS_KILL += item.PEDS_KILL;
+      summaries[month].PEDS_INJD += item.PEDS_INJD;
+      summaries[month].PERS_KILL += item.PERS_KILL;
+      summaries[month].PERS_INJD += item.PERS_INJD;
+      summaries[month].NUM_COLS += item.NUM_COLS;
+    });
+
+    const summaryArray = Object.values(summaries);
+
+    res.json(summaryArray);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
