80d0e0f0f8af181032bdf3446b07f451eec86ab0
Joseph Adams <joseph.adams@blueyonder.co.uk>
2023-09-24 20:28:57 +0100

    new query - but wrong

--- consolodateView.js
+++ consolodateView.js
@@ -5,8 +5,43 @@ const bigquery = new BigQuery();
 async function consolodateViews(tableName, weatherView) {
   console.log("Creating final conslodated table");
 
-  const query = `CREATE TABLE \`uhi-assignment-1.assignment.x4-collated_data\` AS SELECT day, year, mo, da, collision_date,  NEIGHBORHOOD, LAT, LONG,temp, dewp, slp, visib, wdsp, mxpsd, gust, max, min, prcp, sndp, fog, CYCLISTS_KILLED, CYCLISTS_INJURED, MOTORISTS_KILLED, MOTORISTS_INJURED, PEDS_KILLED, PEDS_INJURED,PERSONS_KILLED, PERSONS_INJURED, NUM_COLLISIONS FROM \`uhi-assignment-1.assignment.${weatherView}\` as weather,
-\`uhi-assignment-1.assignment.${tableName}\` as complaints WHERE complaints.collision_date = weather.date`;
+  const query = `
+  CREATE TABLE \`uhi-assignment-1.assignment.x4-collated_data\` AS
+  SELECT
+    DATE,
+    BOROUGH,
+    WEEKDAY,
+    YEAR,
+    MONTH,
+    DAY,
+    COLLISION_DATE,
+    TEMP,
+    DEWP,
+    SLP,
+    VISIB,
+    WDSP,
+    MXPSD,
+    GUST,
+    MAX,
+    MIN,
+    PRCP,
+    SNDP,
+    FOG,
+    CYCLISTS_KILLED,
+    CYCLISTS_INJURED,
+    MOTORISTS_KILLED,
+    MOTORISTS_INJURED,
+    PEDS_KILLED,
+    PEDS_INJURED,
+    PERSONS_KILLED,
+    PERSONS_INJURED,
+    NUM_COLLISIONS
+  FROM
+    \`uhi-assignment-1.assignment.${weatherView}\` AS weather,
+    \`uhi-assignment-1.assignment.${tableName}\` AS complaints
+  WHERE
+    complaints.collision_date = weather.date
+`;
 
   // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
   const options = {
@@ -21,13 +56,8 @@ async function consolodateViews(tableName, weatherView) {
 
   // Wait for the query to finish
   const [rows] = await job.getQueryResults();
-  console.log(`-->XXXXX created successfully.`);
+  console.log(`-->Collated table created successfully.`);
 
   return [rows];
-  // Print the results
-  // console.log("Rows:");
-  // rows.forEach((row) => console.log(row));
-  //   res.json(rows);
-  // }
 }
 module.exports = { consolodateViews };
