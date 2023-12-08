666b159d317221fa350f0759ed9eb89ada3e4eeb
Joseph Adams <joseph.adams@blueyonder.co.uk>
2023-12-08 20:36:50 +0000

    remove all cors

--- server.js
+++ server.js
@@ -27,14 +27,14 @@ const mongoose = require("mongoose");
 const app = express();
 const port = process.env.port || 8080;
 
-const corsOption = {
-  origin: [
-    "http://localhost:3000",
-    "http://nycfrontend-19000170.azurewebsites.net/",
-  ],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-};
+// const corsOption = {
+//   origin: [
+//     "http://localhost:3000",
+//     "http://nycfrontend-19000170.azurewebsites.net/",
+//   ],
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+// };
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -49,7 +49,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.use(corsOption);
+// app.use(corsOption);
 
 mongoose.connect(
   "mongodb+srv://testDBUser:gd5R7PJ75MJXqrXN@webervice.yhwacfo.mongodb.net/AccidentData?retryWrites=true&w=majority"
